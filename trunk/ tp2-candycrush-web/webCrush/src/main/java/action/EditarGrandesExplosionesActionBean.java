01907fd8269c1f67df7f8676d08dc50ea9f1bba3
dimegliofernando@gmail.com <dimegliofernando@gmail.com@c016e9df-22e9-5f82-e7ba-d41baaa9dd3c>
2013-10-23 00:17:22 +0000

    boton editar objetivo

---
+++ trunk/ tp2-candycrush-web/webCrush/src/main/java/action/EditarGrandesExplosionesActionBean.java
@@ -0,0 +1,40 @@
+package action;
+
+
+import Tp.CandyCrush.GrandesExplosiones;
+import net.sourceforge.stripes.action.Before;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.HandlesEvent;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.SessionScope;
+import net.sourceforge.stripes.controller.LifecycleStage;
+
+@SessionScope
+public class EditarGrandesExplosionesActionBean extends EditarObjetivoActionBean {
+	private GrandesExplosiones objetivo;
+	
+	@Before(stages=LifecycleStage.BindingAndValidation)
+	public void iniciarObjetivo(){
+		setObjetivo((GrandesExplosiones) this.getContext().getRequest().getSession().getAttribute("objetivo"));
+	}
+	
+	public GrandesExplosiones getObjetivo() {
+		return objetivo;
+	}
+
+	public void setObjetivo(GrandesExplosiones objetivo) {
+		this.objetivo = objetivo;
+	}
+	
+	@DefaultHandler
+	public Resolution view() {
+		return new ForwardResolution("editarGrandesExplosiones.jsp");
+	}
+
+	@HandlesEvent("agregarObjetivo")
+	public Resolution agregarObjetivo(){
+		this.getContext().getRequest().getSession().setAttribute("objetivo",null);
+		return new ForwardResolution(ConfigurarActionBean.class);
+	}
+}
---
+++ trunk/ tp2-candycrush-web/webCrush/src/main/java/action/EditarObjetivoActionBean.java
@@ -0,0 +1,21 @@
+package action;
+
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.HandlesEvent;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.SessionScope;
+
+@SessionScope
+public abstract class EditarObjetivoActionBean extends BaseActionBean {
+	
+	public abstract Resolution agregarObjetivo();
+	
+	@HandlesEvent("cancelarObjetivo")
+	public Resolution cancelarObjetivo(){
+		
+		this.getContext().getRequest().getSession().setAttribute("objetivo",null);
+				
+		return new ForwardResolution(ConfigurarActionBean.class);
+	}
+	
+}
